188f273004e0464319cf3a88823bb1e32dfe5b78
Nas <nasiopapas@gmail.com>
2024-08-28 18:57:56 +1000

    initial commit

---
+++ src/main/kotlin/com/nas/webresume/WebResumeApplication.kt
@@ -0,0 +1,11 @@
+package com.nas.webresume
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebResumeApplication
+
+fun main(args: Array<String>) {
+	runApplication<WebResumeApplication>(*args)
+}
---
+++ src/test/kotlin/com/nas/webresume/WebResumeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.nas.webresume
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebResumeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
