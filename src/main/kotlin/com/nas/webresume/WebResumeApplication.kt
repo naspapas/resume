ba5b61c1e16de59dea33f9ac1bb3924b9d2a3a28
Nas <nasiopapas@gmail.com>
2024-08-28 19:03:32 +1000

    initial commit

--- src/main/kotlin/com/nas/webresume/WebResumeApplication.kt
+++ src/main/kotlin/com/nas/webresume/WebResumeApplication.kt
@@ -2,10 +2,54 @@ package com.nas.webresume
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.stereotype.Service
+import org.springframework.jdbc.core.JdbcTemplate
+import java.util.UUID
+import org.springframework.jdbc.core.query
+import org.springframework.web.bind.annotation.*
+
 
 @SpringBootApplication
 class WebResumeApplication
 
 fun main(args: Array<String>) {
 	runApplication<WebResumeApplication>(*args)
+}
+
+@RestController
+class MessageController(val service: MessageService) {
+	@GetMapping("/")
+	fun index(): List<Message> = service.findMessages()
+
+	@GetMapping("/{id}")
+	fun index(@PathVariable id: String): List<Message> =
+		service.findMessageById(id)
+
+	@PostMapping("/")
+	fun post(@RequestBody message: Message) {
+		service.save(message)
+	}
+}
+
+data class Message (val id: String?, val text: String)
+
+
+@Service
+class MessageService(val db: JdbcTemplate) {
+
+	fun findMessages(): List<Message> = db.query("select * from messages") { response, _ ->
+		Message(response.getString("id"), response.getString("text"))
+	}
+
+	fun findMessageById(id: String): List<Message> = db.query("select * from messages where id = ?", id) { response, _ ->
+		Message(response.getString("id"), response.getString("text"))
+	}
+
+	fun save(message: Message) {
+		val id = message.id ?: UUID.randomUUID().toString()
+		db.update(
+			"insert into messages values ( ?, ? )",
+			id, message.text
+		)
+	}
 }
